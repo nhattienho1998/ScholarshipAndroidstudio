57804c6dd4c9bd49ccd3fd64d3a525a802d8d4f5
TienHNN <nhattienho1998@gmail.com>
2020-01-04 17:31:07 +0700

    FirstCommit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private ArticlesFragment articlesFragment;
+    private ScholarshipsFragment scholarshipsFragment;
+    private NotificationFragment notificationFragment;
+    private AccountFragment accountFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        articlesFragment = new ArticlesFragment();
+        scholarshipsFragment = new ScholarshipsFragment();
+        notificationFragment = new NotificationFragment();
+        accountFragment = new AccountFragment();
+
+        setFragment(homeFragment);
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_articles:
+                        setFragment(articlesFragment);
+                        return true;
+                    case R.id.nav_scholarships:
+                        setFragment(scholarshipsFragment);
+                        return true;
+                    case R.id.nav_notif:
+                        setFragment(notificationFragment);
+                        return true;
+                    case R.id.nav_account:
+                        setFragment(accountFragment);
+                        return true;
+
+                        default:
+                            return false;
+
+
+                }
+            }
+        });
+    }
+
+    private  void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
+    }
+}
